15f7e3111bc9d12140203fec492cb0accec42195
Sufiana-A <sufi.arum24@gmail.com>
2023-12-30 15:58:17 +0700

    update login

--- resources/views/pages/login.blade.php
+++ resources/views/pages/login.blade.php
@@ -15,8 +15,8 @@
         <form action="" method="post">
             @csrf
             <div class="mb-3">
-                <label for="exampleInputEmail1" class="form-label">Username/Email address</label>
-                <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Masukan username/email terdaftar..." name="email">
+                <label for="exampleInputEmail1" class="form-label">Email address</label>
+                <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Masukan email terdaftar..." name="email">
             </div>
             <div class="mb-3">
                 <label for="exampleInputPassword1" class="form-label">Password</label>
